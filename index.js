6c660f22e264373e62708f206b6b76a1e17e324f
kaveri Arkasali <arkasalikaveri@gmail.com>
2021-08-04 18:20:49 +0530

    update

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/",(req,res) =>{
-      res.send("please keep your mind straight kaveri..........")
-})
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT);
