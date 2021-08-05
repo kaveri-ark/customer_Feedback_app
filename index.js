1d4b9bd4fe80245c7ccbc6d5185c9bd035bcbf8f
kaveri Arkasali <arkasalikaveri@gmail.com>
2021-08-05 17:22:10 +0530

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const express = require("express");
+const  googleStragey = require("passport-google-oauth20").Strategy;
+const  keys = require("./customer_Feedback_app/config/keys");
+
+
+
+const app = express();
+
+passport.use(new googleStragey({
+    clientID: keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'auth/google/callback'
+},
+     accessToken=>{
+         console.log("access token")
+     }
+));
+
+app.get( '/auth/google', passport.authenticate('google',{
+scope:['profile','email']
+}))
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT)
